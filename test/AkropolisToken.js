1bcdcd50ca39f519a4d5eb9eac18fde84e2efcd4
jakub-wojciechowski <jakub@alice.si>
2018-01-06 13:03:56 +0100

    Basics test of Akropolis Token config

---
+++ test/AkropolisToken.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const AkropolisToken = artifacts.require('./AkropolisToken.sol')
+
+const BigNumber = web3.BigNumber;
+
+const should = require('chai')
+	.use(require('chai-as-promised'))
+	.use(require('chai-bignumber')(BigNumber))
+	.should()
+
+contract('Akropolis Token', function ([owner, holder]) {
+
+	let token;
+
+	beforeEach(async function () {
+		token = await AkropolisToken.new()
+	});
+
+	it('should have the correct setup', async function () {
+		(await token.name()).should.be.equal("Akropolis Token");
+		(await token.decimals()).should.be.bignumber.equal(18);
+		(await token.symbol()).should.be.equal("AKR");
+		(await token.version()).should.be.equal("AKR 1.0");
+	});
+
+})
