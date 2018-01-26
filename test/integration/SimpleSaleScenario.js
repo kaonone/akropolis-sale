3d5d0c6a76b8145c934ae094708f421117b479a4
Victor Wiebe <vw@akropolis.io>
2018-01-26 14:02:10 -0800

    Integration for running the sale and then buying the tokens

--- test/integration/SimpleSaleScenario.js
+++ test/integration/SimpleSaleScenario.js
@@ -19,13 +19,15 @@ function ether (n) {
 	return new web3.BigNumber(web3.toWei(n, 'ether'));
 }
 
-contract('Akropolis TGE Scenario', function ([owner, admin, wallet, buyer1, buyer2, buyer3, bonusBuyer4,
+contract('Akropolis TGE Scenario', function ([owner, admin, wallet, buyer1, buyer2, buyer3,
 																						presaleAllocations, teamAllocations, advisorsAllocations,
 																						reserveFund, bountyFund, developmentFund]) {
 
 	const ALLOCATED_VALUE = 100;
 	const ALLOCATED_VESTING = 200;
 	const VESTING_PERIOD = 1000;
+
+	const CONTRIBUTION_AMOUNT = ether(1);
 
 	let token, crowdsale, whitelist, config, allocations;
 	let startTime, endTime, afterEndTime;
@@ -79,18 +81,35 @@ contract('Akropolis TGE Scenario', function ([owner, admin, wallet, buyer1, buye
 
 	it('should deploy crowdsale and connect to token and allocations contracts', async function() {
 		crowdsale = await AkropolisCrowdsale.new(startTime, endTime, wallet, whitelist.address, config.address).should.be.fulfilled;
+		await crowdsale.setAdmin(admin);
 		await token.transferOwnership(crowdsale.address).should.be.fulfilled;
 		await crowdsale.setToken(token.address).should.be.fulfilled;
+		await crowdsale.setBaseCap(ether(3), {from: owner}).should.be.fulfilled;
+		await crowdsale.setMaxCap(ether(10), {from: owner}).should.be.fulfilled;
+		await crowdsale.setRoundDuration(duration.days(1), {from: owner}).should.be.fulfilled;
 	});
 
 
 	it('should sell tokens to whitelisted users', async function() {
-
+		await increaseTimeTo(startTime + duration.days(1));
+		await crowdsale.buyTokens(buyer1, {from: buyer1, value: CONTRIBUTION_AMOUNT}).should.be.fulfilled;
+		await crowdsale.buyTokens(buyer2, {from: buyer2, value: CONTRIBUTION_AMOUNT}).should.be.fulfilled;
+		await crowdsale.buyTokens(buyer3, {from: buyer3, value: CONTRIBUTION_AMOUNT}).should.be.fulfilled;
 	});
 
 
 	it('should finalize crowdsale', async function() {
 
+		await increaseTimeTo(endTime + 1);
+
+		await crowdsale.setPresaleAllocations(presaleAllocations, {from: owner});
+		await crowdsale.setTeamAllocations(teamAllocations, {from: owner});
+		await crowdsale.setAdvisorsAllocations(advisorsAllocations, {from: owner});
+		await crowdsale.setReserveFund(reserveFund, {from: owner});
+		await crowdsale.setBountyFund(bountyFund, {from: owner});
+		await crowdsale.setDevelopmentFund(developmentFund, {from: owner});
+
+		await crowdsale.finalize({from: owner}).should.be.fulfilled;
 	});
 
 
