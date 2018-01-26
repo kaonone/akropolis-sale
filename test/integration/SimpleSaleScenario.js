229b493153dc6dd9a4d601dd8925c7cf138d3c6a
Victor Wiebe <vw@akropolis.io>
2018-01-26 09:18:24 -0800

    Integration test setup and initial contracts registration for allocated investors

--- test/integration/SimpleSaleScenario.js
+++ test/integration/SimpleSaleScenario.js
@@ -1,11 +1,12 @@
-import { advanceBlock } from './tools/advanceToBlock';
-import { increaseTimeTo, duration } from './tools/increaseTime';
-import latestTime from './tools/latestTime';
+import { advanceBlock } from '.././tools/advanceToBlock';
+import { increaseTimeTo, duration } from '.././tools/increaseTime';
+import latestTime from '.././tools/latestTime';
 
 const AkropolisToken = artifacts.require('./AkropolisToken.sol');
 const AkropolisCrowdsale = artifacts.require('./AkropolisCrowdsale.sol');
 const Whitelist = artifacts.require('./Whitelist.sol');
-const SaleConfigurationMock = artifacts.require('./SaleConfigurationMock.sol');
+const SaleConfiguration = artifacts.require('./SaleConfiguration.sol');
+const AllocationsManager = artifacts.require('./AllocationsManager.sol');
 
 const BigNumber = web3.BigNumber;
 
@@ -18,37 +19,47 @@ function ether (n) {
 	return new web3.BigNumber(web3.toWei(n, 'ether'));
 }
 
-contract('Akropolis TGE Scenario', function ([owner, admin, buyer, wallet, bonusBuyer1, bonusBuyer2, bonusBuyer3, bonusBuyer4,
+contract('Akropolis TGE Scenario', function ([owner, admin, buyer1, buyer2, buyer3, bonusBuyer4,
 																						presaleAllocations, teamAllocations, advisorsAllocations,
 																						reserveFund, bountyFund, developmentFund]) {
 
-	let token, crowdsale, whitelist, config;
+	const ALLOCATED_VALUE = 100;
+	const ALLOCATED_VESTING = 200;
+	const VESTING_PERIOD = 1000;
+
+	let token, crowdsale, whitelist, config, allocations;
 	let startTime, endTime, afterEndTime;
 
 
-
 	it('should deploy AET token', async function () {
-
+		token = await AkropolisToken.new().should.be.fulfilled;
 	});
 
 
 	it('should deploy Whitelist', async function () {
-
+		whitelist = await Whitelist.new().should.be.fulfilled;
 	});
 
 
 	it('should register 3 users to the whitelist', async function () {
-
+		await whitelist.setAdmin(admin);
+		await whitelist.addToWhitelist(buyer1, {from: admin}).should.be.fulfilled;
+		await whitelist.addToWhitelist(buyer2, {from: admin}).should.be.fulfilled;
+		await whitelist.addToWhitelist(buyer3, {from: admin}).should.be.fulfilled;
 	});
 
 
 	it('should deploy pre-sale allocations', async function() {
-
+		allocations = await AllocationsManager.new().should.be.fulfilled;
 	});
 
 
 	it('should register 3 investors', async function() {
+		await allocations.setAdmin(admin);
 
+		await allocations.registerAllocation(buyer1, ALLOCATED_VALUE, 0, 0, {from: admin}).should.be.fulfilled;
+		await allocations.registerAllocation(buyer2, ALLOCATED_VALUE, ALLOCATED_VESTING, VESTING_PERIOD, {from: admin}).should.be.fulfilled;
+		await allocations.registerAllocation(buyer3, ALLOCATED_VALUE, ALLOCATED_VESTING, VESTING_PERIOD, {from: admin}).should.be.fulfilled;
 	});
 
 
