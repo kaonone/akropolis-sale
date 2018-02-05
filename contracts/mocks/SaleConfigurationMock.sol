5f963838d3e57a7b5c9c77b1fea55fb23063b2f7
Victor Wiebe <vw@akropolis.io>
2018-02-05 13:27:02 -0800

    Get rid of constant declaration of variable in sales config mock

--- contracts/mocks/SaleConfigurationMock.sol
+++ contracts/mocks/SaleConfigurationMock.sol
@@ -7,7 +7,7 @@ pragma solidity ^0.4.18;
 contract SaleConfigurationMock {
 
     uint256 public AET_RATE = 10;
-    uint256 public constant HARD_CAP = 10000 ether;
+    uint256 public HARD_CAP = 10000 ether;
     uint256 public constant MAX_ALLOCATION_VALUE = 1000 ether;
 
     uint256 public constant PUBLIC_SALE_SUPPLY = 10000 ether;
