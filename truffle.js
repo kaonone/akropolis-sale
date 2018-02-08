d5a98931f467f5b6178def4dc86b2654f9c57229
Victor Wiebe <vw@akropolis.io>
2018-02-08 11:55:58 -0800

    Add truffle kovan settings, and small whitelist change

--- truffle.js
+++ truffle.js
@@ -1,6 +1,8 @@
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register');
 require('babel-polyfill');
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "12 words";
 
 module.exports = {
     networks: {
@@ -21,6 +23,12 @@ module.exports = {
                 enabled: true,
                 runs: 200
             }
-        }
+        },
+		kovan: {
+			provider: function() {
+				return new HDWalletProvider(mnemonic, "https://kovan.infura.io/tokenkey")
+			},
+			network_id: 42
+		}
     }
 };
