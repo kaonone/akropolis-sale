ff455cb641a363e80ba5945384e74bba7a0337ce
Victor Wiebe <vw@akropolis.io>
2018-02-14 13:21:51 -0800

    Fix up public sale cap reach test

--- test/integration/ReachRound1PublicSaleCapScenario.js
+++ test/integration/ReachRound1PublicSaleCapScenario.js
@@ -20,35 +20,35 @@ function ether (n) {
 	return new web3.BigNumber(web3.toWei(n, 'ether'));
 }
 
-//This integration test seeks to explore reaching the public sale token sales cap
-//This test affirms that if we can finalize the public sale if we reach the public sale token sales limit
-//In this test we reach the limit in Round 1
-contract('Akropolis Reach Public Sale Cap Round 1 Scenario', function ([owner, admin, wallet, buyer1, buyer2, buyer3, buyer4, investor1, investor2, investor3,
+//This integration test seeks to explore reaching the hard cap for ether sent in exchange for tokens
+//This test affirms that if we can finalize the public sale if we reach the ether sales limit
+//In this test we reach the sales limit in Round 1
+contract('Akropolis Round 1 Public Sale Cap Reach Scenario', function ([owner, admin, wallet, buyer1, buyer2, investor1, investor2, investor3,
 																						reserveFund, bountyFund, developmentFund, unknown]) {
 
-	const CONTRIBUTION_AMOUNT = ether(1);
+	const ALLOCATED_VALUE = 100;
+	const ALLOCATED_VESTING = 200;
+	const VESTING_PERIOD = duration.days(100);
+	const VESTING_CLIFF = duration.days(25);
+
 
 	let token, crowdsale, whitelist, config;
 	let presaleAllocations, teamAllocations, advisorsAllocations;
 	let startTime, endTime, afterEndTime;
-	let tokenBuyerAmount;
 
 	before(async function () {
 		// Advance to the next block to correctly read time in the solidity "now" function interpreted by testrpc
 		await advanceBlock();
 
 		startTime = latestTime() + duration.weeks(1);
-		endTime = startTime + duration.days(4);
-		afterEndTime = endTime + duration.seconds(1);token =
-
-		await AkropolisToken.new();
+		endTime = startTime + duration.days(9);
+		afterEndTime = endTime + duration.seconds(1);
+		token = await AkropolisToken.new();
 		await token.pause();
 		whitelist = await Whitelist.new();
 		await whitelist.setAdmin(admin);
 		await whitelist.addToWhitelist(buyer1, 1, {from: admin});
 		await whitelist.addToWhitelist(buyer2, 1, {from: admin});
-		await whitelist.addToWhitelist(buyer3, 1, {from: admin});
-		await whitelist.addToWhitelist(buyer4, 1, {from: admin});
 
 		presaleAllocations = await AllocationsManager.new();
 		await presaleAllocations.setToken(token.address);
@@ -62,34 +62,45 @@ contract('Akropolis Reach Public Sale Cap Round 1 Scenario', function ([owner, a
 		await advisorsAllocations.setToken(token.address);
 		await advisorsAllocations.setAdmin(admin);
 
-		config = await SaleConfigurationMock.new();
+		//Register some investors
+		await presaleAllocations.registerAllocation(investor1, ALLOCATED_VALUE, ALLOCATED_VESTING, VESTING_CLIFF, VESTING_PERIOD, {from: admin});
+		await presaleAllocations.registerAllocation(investor2, (ALLOCATED_VALUE * 2), (ALLOCATED_VESTING * 10), VESTING_CLIFF, (VESTING_PERIOD * 2), {from: admin});
+		await presaleAllocations.registerAllocation(investor3, ALLOCATED_VALUE, 0, 0, 0, {from: admin});
 	});
 
 
 	it('should deploy crowdsale and connect to token and allocations contracts', async function() {
+		config = await SaleConfigurationMock.new().should.be.fulfilled;
+		await config.setPUBLIC_SALE_SUPPLY(ether(200)).should.be.fulfilled;
 		crowdsale = await AkropolisCrowdsale.new(startTime, endTime, wallet, whitelist.address, config.address).should.be.fulfilled;
-		await config.setPUBLIC_SALE_SUPPLY(ether(12)).should.be.fulfilled;
-		await crowdsale.setAdmin(admin);
 		await token.transferOwnership(crowdsale.address).should.be.fulfilled;
 		await crowdsale.setToken(token.address).should.be.fulfilled;
-		await crowdsale.setBaseCap(ether(3), {from: owner}).should.be.fulfilled;
-		await crowdsale.setMaxCap(ether(10), {from: owner}).should.be.fulfilled;
-		await crowdsale.setRoundDuration(duration.days(1), {from: owner}).should.be.fulfilled;
+	});
+
+
+	it('should not finalize the sale before the start time', async function () {
+		await crowdsale.finalize({from: owner}).should.be.rejectedWith('revert');
+	});
+
+
+	it('should not finalize the sale before the end of token sale or reach the hard cap', async function () {
+		await crowdsale.finalize({from: owner}).should.be.rejectedWith('revert');
 	});
 
 
 	it('should sell tokens to whitelisted users during round 1', async function() {
-		tokenBuyerAmount = (await config.AET_RATE()).mul(CONTRIBUTION_AMOUNT);
+		let tokenBuyerAmountRound1 = (await config.AET_RATE()).mul(ether(10));
 		await increaseTimeTo(startTime);
 		(await crowdsale.getCurrentRound()).should.be.bignumber.equal(1);
-		await crowdsale.buyTokens(buyer1, {from: buyer1, value: CONTRIBUTION_AMOUNT}).should.be.fulfilled;
+		await crowdsale.buyTokens(buyer1, {from: buyer1, value: ether(10)}).should.be.fulfilled;
+		await crowdsale.buyTokens(buyer2, {from: buyer2, value: ether(10)}).should.be.fulfilled;
 
-		let tokenBuyerAmountRound1 = tokenBuyerAmount.mul(1.2);
 		(await token.balanceOf(buyer1)).should.be.bignumber.equal(tokenBuyerAmountRound1);
+		(await token.balanceOf(buyer2)).should.be.bignumber.equal(tokenBuyerAmountRound1);
 	});
 
 
-	it('should finalize crowdsale when public sale cap is reached', async function() {
+	it('should finalize crowdsale after reaching public sale cap', async function() {
 		await advanceBlock();
 		await crowdsale.setPresaleAllocations(presaleAllocations.address, {from: owner});
 		await crowdsale.setTeamAllocations(teamAllocations.address, {from: owner});
@@ -101,14 +112,16 @@ contract('Akropolis Reach Public Sale Cap Round 1 Scenario', function ([owner, a
 
 		await crowdsale.finalize({from: owner}).should.be.fulfilled;
 
-		//Test reserve fund balance has nothing added to it from crowdsale, is just reserve fund value
-		(await token.balanceOf(reserveFund)).should.be.bignumber.equal((await config.RESERVE_FUND_VALUE()));
+		//Test reserve fund
+		let sold = await crowdsale.tokensSold();
+		let supply = await config.PUBLIC_SALE_SUPPLY();
+		let unsold = supply.sub(sold);
+		(await token.balanceOf(reserveFund)).should.be.bignumber.equal((await config.RESERVE_FUND_VALUE()).add(unsold));
 	});
 
 
 	it('should allow for transfer of tokens', async function () {
 		await token.transfer(unknown, 1, {from: buyer1}).should.be.fulfilled;
-
 		await token.approve(unknown, 1, {from: buyer1}).should.be.fulfilled;
 		await token.transferFrom(buyer1, unknown, 1, {from: unknown}).should.be.fulfilled;
 	})
