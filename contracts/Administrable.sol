0ae8114a6f3213d0f4f83bcef692324ef918bfd6
jakub-wojciechowski <jakub@alice.si>
2018-02-15 13:07:21 +0100

    Update description.

--- contracts/Administrable.sol
+++ contracts/Administrable.sol
@@ -7,7 +7,12 @@ import "zeppelin-solidity/contracts/token/SafeERC20.sol";
 import "./AkropolisToken.sol";
 import "./LinearTokenVesting.sol";
 
-
+/**
+ * @title Administrable
+ * @dev Contract defines a role of an administrator with lesser privileges than the owner.
+ * The administrator may perform oranizational task without having a decisive power over
+ * token or funds transfer.
+ */
 contract Administrable is Ownable {
 
     event AdminChanged(address indexed previousAdmin, address indexed newAdmin);
@@ -23,6 +28,9 @@ contract Administrable is Ownable {
         _;
     }
 
+    /**
+    * @dev Owner is allowed to replace the admin at any given time
+    */
     function setAdmin(address _admin) public onlyOwner {
         require(address(_admin) != 0x0);
         AdminChanged(admin, _admin);
