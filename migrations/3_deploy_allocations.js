95d8a3b60b15459fd70ab1372f726bdee418d3bb
Victor Wiebe <vw@akropolis.io>
2018-02-19 06:46:32 -0800

    Reflect 3 seperate allocations contract deployments in script

--- migrations/3_deploy_allocations.js
+++ migrations/3_deploy_allocations.js
@@ -2,8 +2,17 @@ const Allocations = artifacts.require('./AllocationsManager.sol');
 
 module.exports = async function(deployer, network, accounts) {
 	await deployer.deploy(Allocations);
-	let allocations = await Allocations.deployed();
-	await allocations.setAdmin(accounts[0]);
+	let presaleAllocations = await Allocations.deployed();
+	await presaleAllocations.setAdmin(accounts[0]);
+
+	await deployer.deploy(Allocations);
+	let teamAllocations = await Allocations.deployed();
+	await teamAllocations.setAdmin(accounts[0]);
+
+	await deployer.deploy(Allocations);
+	let advisorsAllocations = await Allocations.deployed();
+	await advisorsAllocations.setAdmin(accounts[0]);
+
 };
 
 
