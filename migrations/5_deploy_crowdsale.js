1c572b81d55e3e4f1ae3701565989a7b7aa873ee
Victor Wiebe <vw@akropolis.io>
2018-03-22 02:52:28 -0800

    Remove code for logging the deployment

--- migrations/5_deploy_crowdsale.js
+++ migrations/5_deploy_crowdsale.js
@@ -26,26 +26,4 @@ module.exports = function(deployer, network, accounts) {
 		instance.setTeamAllocations(process.deployment.TeamAllocations);
 	});
 
-	//Capture a log of this full migration crowdsale and write to JSON
-	const fs = require('fs');
-	const logFolder = './migrations/migrationLogs';
-
-	var timestamp = new Date().toISOString().replace(/T/, '_').replace(/:/, '_');
-	var jsonPackage = JSON.stringify(process.deployment);
-	console.log("Creating log file from process variable");
-	fs.readdir(logFolder, (err, files) => {
-		if(err) throw err;
-		var index;
-		if (files) {
-			index = files.length;
-		}
-		else {
-			index = 0;
-		}
-		fs.writeFile(logFolder + "/" + index +'_Migration_' + timestamp, jsonPackage, function(err){
-			if(err) throw err;
-			console.log("Successfully created log file");
-		});
-	});
-
 };
