07a915a87cd7ad8d4296855d95ba39f63eff4ea0
Victor Wiebe <vw@akropolis.io>
2018-03-22 02:45:54 -0800

    Git rid of kovan in truffle to test if truffle test works in circle ci

--- truffle.js
+++ truffle.js
@@ -1,8 +1,6 @@
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register');
 require('babel-polyfill');
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "12 words";
 
 module.exports = {
     networks: {
@@ -23,14 +21,6 @@ module.exports = {
                 enabled: true,
                 runs: 200
             }
-        },
-		kovan: {
-			provider: function() {
-				return new HDWalletProvider(mnemonic, "https://kovan.infura.io/tokenkey")
-			},
-			network_id: 42,
-			gas: 5000000, // <-- Use this high gas value
-			gasPrice: 0x05
-		}
+        }
     }
 };
