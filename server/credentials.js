d630189614e603a0918afec16b36b33f49fe5dca
Victor Wiebe <vw@akropolis.io>
2018-03-28 04:08:36 -0800

    Add fixes for best practice using dotenv instead of javascript file for environment variables

--- server/credentials.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    host: "YOUR HOST HERE",
-    user: "YOUR USERNAME HERE",
-    password: "YOUR PASSWORD HERE",
-    database: "YOUR DATABASE HERE"
-};
--- server/importWhitelist.js
+++ server/importWhitelist.js
@@ -1,25 +1,30 @@
+require('dotenv').config();
 var mysql = require('mysql');
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require("body-parser"); // Body parser for fetch posted data
-var credentials = require('./credentials');
-
 var app = module.exports= express();
 //Setup a config
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//The connection details can be specified in .env file
 var connection = mysql.createConnection({
-	host: credentials.host,
-	user: credentials.user,
-	password: credentials.password,
-	database: credentials.database
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
 });
 connection.query('USE akropoli_db1', function (err) {
 	if (err) throw err;
 });
 
-app.get('/kycReadyUsers', cors(), function (req, res) {
+var corsOptions = {
+	origin: 'http://localhost', // This can only be pinged locally for now
+	optionsSuccessStatus: 200,
+};
+
+app.get('/kycReadyUsers', cors(corsOptions), function (req, res) {
 	var data = {
 		"Data":""
 	};
@@ -38,7 +43,7 @@ app.get('/kycReadyUsers', cors(), function (req, res) {
 //Update individual tiers of users when they are updated from the admin panel
 //Example input, Tier: 1 and EthAddress : 0x00...
 
-app.post('/updateEthAddressTier', cors(), function (req, res) {
+app.post('/updateEthAddressTier', cors(corsOptions), function (req, res) {
 	var sqlQuery = 'UPDATE whitelist set Tier = \''+ req.body.Tier +'\' WHERE EthAddress =\'' + req.body.EthAddress+ '\'';
 	console.log(sqlQuery);
 	connection.query(sqlQuery,
@@ -50,7 +55,7 @@ app.post('/updateEthAddressTier', cors(), function (req, res) {
 
 //Set added to smart contract to 1 for true, where eth address is equal to any number of passed in comma delimited eth addresses
 //Example input EthAddresses: '0x00...', '0x11...', '0x22..'
-app.post('/updateAddedToSmartContractEntries', cors(), function (req, res) {
+app.post('/updateAddedToSmartContractEntries', cors(corsOptions), function (req, res) {
 	connection.query('UPDATE whitelist set AddedToSmartContract = \'1\' WHERE EthAddress in ('+ req.body.EthAddresses + ')',
 		function (err, result) {
 		if (err) throw err;
