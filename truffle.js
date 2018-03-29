c00154920c97040db0b0c2703c789b39ec9cd07e
jakub-wojciechowski <jakub@alice.si>
2018-03-29 21:07:51 +0200

    Updated migration config

--- truffle.js
+++ truffle.js
@@ -1,6 +1,8 @@
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register');
 require('babel-polyfill');
+var HDWalletProvider = require("./tools/akropolis-hdwallet-provider");
+const config = require('./test-config.json');
 
 module.exports = {
     networks: {
@@ -15,6 +17,14 @@ module.exports = {
             port: 8555,
             gas: 0xfffffffffff, // <-- Use this high gas value
             gasPrice: 0x01      // <-- Use this low gas price
+        },
+        kovan: {
+          provider: function() {
+            return new HDWalletProvider(config.mnemonic, "http://localhost:8545", 0, 3)
+          },
+          network_id: 42,
+          gas: 7000000, // <-- Use this high gas value
+          gasPrice: 0x05
         },
         solc: {
             optimizer: {
