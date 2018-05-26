0b49aecd1feeab67dce8de1fc9fd31f24c405eea
victorwiebe <vw@akropolis.io>
2018-05-26 15:54:19 -0400

    Update Crowdsale Sample Values to reflect website token amounts

--- contracts/SaleConfiguration.sol
+++ contracts/SaleConfiguration.sol
@@ -13,20 +13,21 @@ contract SaleConfiguration {
     uint8 public constant DECIMALS = 18;
     uint256 public constant DECIMALS_FACTOR = 10**uint256(DECIMALS);
 
-    //TODO: Update and verify values before going live
+    //Disclaimer: This file is subject to change prior to the ICO. 
+    //It will be updated with values just before Crowdsale goes live
 
     uint256 public constant AKT_RATE = 10000;
-    uint256 public constant HARD_CAP = 6000 ether;
+    uint256 public constant HARD_CAP = 30000 ether;
     uint256 public constant MAX_ALLOCATION_VALUE = DECIMALS_FACTOR * 1000;
 
     uint256 public constant TOTAL_SUPPLY = DECIMALS_FACTOR * 900000000;
-    uint256 public constant PUBLIC_SALE_SUPPLY = DECIMALS_FACTOR * 90000000;
+    uint256 public constant PUBLIC_SALE_SUPPLY = DECIMALS_FACTOR * 180000000;
     uint256 public constant PRESALE_SUPPLY = DECIMALS_FACTOR * 180000000;
     uint256 public constant TEAM_SUPPLY = DECIMALS_FACTOR * 180000000;
-    uint256 public constant ADVISORS_SUPPLY = DECIMALS_FACTOR * 49500000;
+    uint256 public constant ADVISORS_SUPPLY = DECIMALS_FACTOR * 90000000;
 
     uint256 public constant RESERVE_FUND_VALUE = DECIMALS_FACTOR * 180000000;
-    uint256 public constant DEVELOPMENT_FUND_VALUE = DECIMALS_FACTOR * 220500000;
+    uint256 public constant DEVELOPMENT_FUND_VALUE = DECIMALS_FACTOR * 90000000;
 
     uint256 public constant MIN_TIER_1 = 2 ether;
     uint256 public constant MAX_TIER_1 = 10 ether;
