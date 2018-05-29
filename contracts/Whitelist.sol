8a55b340cf5686fedcbb4a5471505e347770e400
victorwiebe <vw@akropolis.io>
2018-05-29 13:13:28 -0400

    Small changes to contracts and update sale config with 15% Public sale as placeholder

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -34,7 +34,6 @@ contract Whitelist is Administrable {
 
     function addMultipleToWhitelist(address[] buyerAddresses, uint8[] buyerTiers) public onlyAdmin {
         require(buyerAddresses.length == buyerTiers.length);
-	require(buyerAddresses.length<=100); //Limit set at 100
         for(uint i=0; i<buyerAddresses.length; i++){
             addToWhitelist(buyerAddresses[i], buyerTiers[i]);
         }
